b060508548b7893b64f4da31842e48531a61b633
Igor P. Leroy <ip.leroy@gmail.com>
2015-12-14 22:47:33 -0200

    Fix quotes in yml file and improves output message

--- generate_test.sh
+++ generate_test.sh
@@ -5,9 +5,11 @@ endpoint=$1
 echo "
 ---
 - config:
-  - testset: "${endpoint}"
+  - testset: \""${endpoint}\""
 
 - test:
-  - name: "Basic get"
-  - url: "/${endpoint}"
+  - name: \""Basic get\""
+  - url: \""/${endpoint}\""
 " > test/${endpoint}.yml
+
+echo "Created ${endpoint} test at test/${endpoint}.yml"
