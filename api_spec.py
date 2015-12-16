a75b7de3f5316f8ac0ef399278ab9cb423bafca7
Igor P. Leroy <ip.leroy@gmail.com>
2015-12-16 11:29:21 -0200

    Move .sh files to src folder

--- api_spec.py
+++ api_spec.py
@@ -11,7 +11,7 @@ def cli():
 @cli.command()
 @click.argument('name', default='api_test')
 def run_tests(name):
-    subprocess.call(['./run_tests.sh', name])
+    subprocess.call(['./src/run_tests.sh', name])
 
 
 @cli.command()
@@ -23,7 +23,7 @@ def recreate_schema(name):
 @cli.command()
 @click.option('--name', help='spec name')
 def generate_test(name):
-    subprocess.call(['./generate_test.sh', name])
+    subprocess.call(['./src/generate_test.sh', name])
 
 
 if __name__ == '__main__':
